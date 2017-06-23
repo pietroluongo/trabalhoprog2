449de7d64c967dd6bc73f74e0a43a904b1098f8a
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2017-06-23 00:23:35 -0300

    Initial Commit

---
+++ headers/tCartela.h
@@ -0,0 +1,27 @@
+/* 
+ * File:   tCartela.h
+ * Author: Gabriel Pietroluongo Nascimento
+ *
+ * Tipo tCartela
+ */
+
+#ifndef TCARTELA_H
+#define TCARTELA_H
+
+#include "tGeradorAle.h"
+
+typedef struct
+{
+    int id;
+    int lin;
+    int col;
+    int nums[lin][col];
+    
+} tCartela;
+
+tCartela CriaCartela(int lin, int col);
+
+void PrintaCartela(tCartela cartela);
+
+#endif /* TCARTELA_H */
+
